fe17b861e1efbe294cf334cb04bd884006ff962b
2303015064 <lizhenhan@sunwoda-evb.com>
2023-03-29 15:19:17 +0800

    start.sh修改，dockerfile小问题修改

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 docker pull zhenyu00/rescue-backend:latest
-docker rm -f rescue||true&&docker run  --name=appName -d -p 8080:8080 zhenyu00/app:latest
+docker rm -f rescue||true&&docker run  --name=rescue -d -p 3088:3088 zhenyu00/rescue-backend:latest
 docker image prune -af
